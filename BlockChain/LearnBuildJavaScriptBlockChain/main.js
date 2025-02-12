4232cc65ff9d748152f3e098d1d8ea10d7373396
sharettleDeveloper <SharettleDeveloper@users.noreply.github.com>
2025-02-12 22:34:21 +0900

    class blockchain

---
+++ BlockChain/LearnBuildJavaScriptBlockChain/main.js
@@ -0,0 +1,14 @@
+class Block {
+  constructor(timestamp, data) {
+    this.index = 0;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = "0";
+    this.hash = this.calculatehash();
+    this.nonce = 0;
+  }
+
+  calculatehash() {}
+
+  mineBlock(difficulty) {}
+}
